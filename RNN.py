7898494c9b0bc009be42d87d778d0d14f536f815
Joseph Luttrell <jluttrell@ucsd.edu>
2015-12-03 23:22:42 -0800

    Moved read file to RNN.py, fixed various silly errors

--- RNN.py
+++ RNN.py
@@ -1,12 +1,12 @@
-from ReadFile import ReadFile
 import numpy as np
+
+num_chars = 256
 
 class RNN:
   '''
   Character level recurrent neural network framework adapted from code from WildML blog
   (www.wildml.com/2015/09/recurrent-neural-networks-tutorial-part-1-introduction-to-rnns/)
   '''
-  num_chars = 256
 
   def __init__(self, num_hidden, temp):
 
@@ -16,7 +16,7 @@ class RNN:
     #input to hidden
     self.W_xh = np.random.uniform(-np.sqrt(1./num_chars), np.sqrt(1./num_chars), (num_hidden, num_chars))
     #hidden to output
-    self.W.ho = np.random.uniform(-np.sqrt(1./num_hidden), np.sqrt(1./num_hidden), (num_chars, num_hidden))
+    self.W_ho = np.random.uniform(-np.sqrt(1./num_hidden), np.sqrt(1./num_hidden), (num_chars, num_hidden))
     #recurrent connection in the hidden
     self.W_hh = np.random.uniform(-np.sqrt(1./num_hidden), np.sqrt(1./num_hidden), (num_hidden, num_hidden))
 
@@ -33,7 +33,7 @@ class RNN:
 
     for t in np.arange(T):
       hidden[t] = np.tanh(self.W_xh[:,x[t]] + np.dot(self.W_hh, hidden[t-1]))
-      out[t] = softmax(np.dot(self.W_ho, s[t]), self.temp)
+      out[t] = softmax(np.dot(self.W_ho, hidden[t]), self.temp)
     return out, hidden
 
   def bptt(self, x, y):
@@ -43,19 +43,20 @@ class RNN:
 
     dLdW_xh = np.zeros(self.W_xh.shape)
     dLdW_ho = np.zeros(self.W_ho.shape)
-    dLdW_hh = np.zeros(self.w_hh.shape)
+    dLdW_hh = np.zeros(self.W_hh.shape)
 
-    delta_out = o - y
+    delta_out = o
+    delta_out[np.arange(len(y)),y] -= 1
 
     for t in np.arange(T)[::-1]:
       dLdW_ho += np.outer(delta_out[t], h[t].T)
-      delta_t = np.dot(W_ho.T, delta_out[t]) * (1 - (h[t] ** 2))
+      delta_t = np.dot(self.W_ho.T, delta_out[t]) * (1 - (h[t] ** 2))
 
       for step in np.arange(max(0, t), t+1)[::-1]:
-        dLdW_hh += np.outer(delta_t, h[s-1])
+        dLdW_hh += np.outer(delta_t, h[step-1])
         dLdW_xh[:,x[step]] += delta_t
 
-        delta_t = np.dot(W_hh.T, delta_t) * (1 - (h[step-1] ** 2))
+        delta_t = np.dot(self.W_hh.T, delta_t) * (1 - (h[step-1] ** 2))
     
     return dLdW_xh, dLdW_ho, dLdW_hh
 
@@ -64,7 +65,7 @@ class RNN:
     ascii_number = np.argmax(out, axis=1)
     return str(unichr(ascii_number))
 
-  def sdg_step(self, x, y, lr):
+  def sgd_step(self, x, y, lr):
     dLdW_xh, dLdW_ho, dLdW_hh = self.bptt(x,y)
     self.W_xh -= dLdW_xh
     self.W_ho -= dLdW_ho
@@ -72,23 +73,42 @@ class RNN:
 
 ### End RNN
 
-def train(model, x, y, lr, num_epochs):
+def train(model, x, lr, sequence_len,num_epochs):
   for epoch in range(num_epochs):
-    for i in range(len(y)):
-      model.sgd_step(x[],y[], lr)
+    i = 0
+    while (i+1+sequence_len) < len(x):
+      model.sgd_step(x[i:i+sequence_len],x[i+1:i+1+sequence_len], lr)
+      i += 1
 
 def softmax(a,temp):
   numer = np.exp(a/temp)
   out = numer / numer.sum()
   return out
 
+def createX(filename):
+  f = open(filename)
+  x = []
+  c = f.read(1)
+
+  while c is not None:
+    if len(c) == 0:
+      break
+    x.append(ord(c))
+    c = f.read(1)
+  return x
+
 def main():
-  text = ReadFile('infile.txt')
-  x,y = text.createXY()
+  filename = 'infile.txt'
+  x = createX(filename)
+
   hidden_size = 10
-  sequence_len = 10
   temp = 1
-  net = RNN(hidden_size, sequence_len, temp)
+  net = RNN(hidden_size, temp)
+
+  sequence_len = 10
+  num_epochs = 100
+  learning_rate = 0.1
+  train(net, x, learning_rate, sequence_len, 100)
 
   print "done"
 
